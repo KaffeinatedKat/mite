952dccf454acda740f2cf34d1330de346503e117
John Crawford <coffeeboi47@protonmail.com>
2023-02-20 23:50:48 -0700

    refactor: document the rest of edit.cpp

--- src/edit.cpp
+++ src/edit.cpp
@@ -131,21 +131,29 @@ int edit::insertMode(file &File, screen &Screen, cursor &Cursor, popup &Popup, c
 
 int edit::commandMode(file &File, screen &Screen, cursor &Cursor, mode &Mode, char &c) {
     int retVal = 0;
+
     if (c == 'i') { //  Insert mode
         Mode = insert; 
+
     } else if (c == 'a') {
         Cursor.column++;
         Screen.cursorChar++;
         Mode = insert;
-    } else if (c == 'x') { //  Delete char
+
+    } else if (c == 'x') { //  Delete char at current index
         File.vect[Screen.cursorLine].erase(Cursor.column - Cursor.offset, 1);
         retVal = 1;
 
     } else if (c == 'j') { //  Cursor down
+        //  Do not move the terminal's cursor "out of bounds" when scrolling
+        //  downwards, or it will not move up again when scrolling back up.
+        //  Check if it's at the bottom and don't move it if it is
         if (!(Cursor.row >= Screen.verticalSize)) { Cursor.row += 1; }
         Screen.cursorLine += 1;
 
     } else if (c == 'k') { //  Cursor up
+        //  Check if the cursor is at the topline to prevent you from
+        //  scrolling up past the first line, as there is no text up there
         if (!(Cursor.row <= 1)) { Cursor.row -= 1; }
         if (!(Screen.cursorLine < 1)) { Screen.cursorLine -= 1; }
 
@@ -154,7 +162,12 @@ int edit::commandMode(file &File, screen &Screen, cursor &Cursor, mode &Mode, ch
         if (!(Cursor.column == Screen.horizontalSize)) { Cursor.column++; }
 
     } else if (c == 'h') { // Cursor left
+        //  Check if the terminal cursor is at the offset value to prevent
+        //  you from scrolling into the line counter
         if (!(Cursor.column == Cursor.offset)) { Cursor.column--; }
+        //  Check if the cursor is the beginning of a line to prevent the
+        //  cursor from being at a negitive index likely causing a crash at
+        //  some point
         if (Screen.cursorChar > 0) { Screen.cursorChar--; }
 
     } else if (c == 'w') { //  Write to file
@@ -162,30 +175,52 @@ int edit::commandMode(file &File, screen &Screen, cursor &Cursor, mode &Mode, ch
         Screen.cmdLine = "File hath been wroteth";
 
     } else if (c == 127) { //  Delete line
+        //  FIXME: this shouldn't exist, does not work with lsp. Needs to
+        //  be replaced with 'dd' when the command line is implemented 
         File.vect.erase(File.vect.begin() + Screen.cursorLine);
 
-    } else if (c == 't') {
+    } 
+
+#ifdef DEBUG
+    else if (c == 't') { //  Debug print the undo stack 
         for (auto& it : undoStack) {
             printf("\n[%d] [%d] [%d] [%s]\n", it.line, it.index, it.action, it.text.c_str());
         }
         exit(0);
 
-    } else if (c == 'u') {
-        if (undoIndex < 0) { return 0; } //  Do not undo if there is nothing to undo
+    } 
+#endif
+
+    else if (c == 'u') { //  Undo
+        //  If the undo index is 0, then when you try to undo you
+        //  get a std::out_of_range, check for this to prevent a crash.
+        //
+        //  Return 0 to signify no change to the document
+        if (undoIndex < 0) { return 0; }
         undo(File, Screen, Cursor);
+
+        //  Undo was exectued and the document has been changed. Return 
+        //  value of 1 to reflect that
         retVal = 1;
 
-    } else if (c == '$') {
+    } else if (c == '$') { //  Move cursor to the end of the current line
         endOfTheLine(File, Screen, Cursor);
+        //  Move the cursor to the end of the screen
         Cursor.column = Cursor.offset + Screen.horizontalSize;
 
-        //  If the line is shorter than the screen
-        //  the terminal cursor will be in the wrong spot
+        //  If the current line is shorter than the horizontal size
+        //  of the terminal the terminal cursor will be in the wrong spot.
+        // 
+        //  Move the terminal cursor to the end of the current line accouting
+        //  for the offset of the line counter
         if ((int) File.vect[Screen.cursorLine].length() < Screen.horizontalSize) {  
             Cursor.column = Cursor.offset + File.vect[Screen.cursorLine].length();
         }
     }
 
+    //  If the cursor's position is greater than the current line (eg. moving 
+    //  up/down to a shorter line from a longer one), move the cursor to the 
+    //  end of the current line to account for it.
     if (Screen.cursorChar > (int) File.vect[Screen.cursorLine].length()) {
        endOfTheLine(File, Screen, Cursor);
     }
@@ -194,7 +229,11 @@ int edit::commandMode(file &File, screen &Screen, cursor &Cursor, mode &Mode, ch
 }
 
 void edit::undo(file &File, screen &Screen, cursor &Cursor) {
-    //  undoStack format [line, index, action (0-1), char]
+    //  undoStack instruction format [line, index, action (0-1), char]
+    //
+    //  FIXME: this is weird and half working. Insert action does not work
+    //  and needs to work with newlines 
+
     undoInstruction Undo = undoStack[undoIndex];
     std::string lineBegin = File.vect[Undo.line].substr(0, Undo.index);
     std::string lineEnd = File.vect[Undo.line].substr(Undo.index);
@@ -202,11 +241,15 @@ void edit::undo(file &File, screen &Screen, cursor &Cursor) {
     Cursor.column = Cursor.offset + Undo.index;
     Screen.cursorLine = Cursor.row - 1;
 
-    if (Undo.action == 1) {  //  Insert a char
+    //  FIXME: this doesnt do anything?
+    if (Undo.action == 1) {  //  Insert action
+        //  Append the undo text to the end of the beginning
+        //  substring
         lineBegin.append(Undo.text.c_str());
         Cursor.column++;
 
-    } else if (Undo.action == 0) { //  Delete a char
+    } else if (Undo.action == 0) { //  Delete action
+        //  Remove the length of the undo text from the undo line
         File.vect[Undo.line].erase(Undo.index, Undo.text.length());
         Cursor.column--;
 
